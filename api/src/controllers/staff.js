4857599e2463b936ba9ad59175311a6b0db29b64
jcariajano <juliocariajano@gmail.com>
2022-09-29 22:00:47 -0500

    rutas y controladores staff

--- api/src/controllers/staff.js
+++ api/src/controllers/staff.js
@@ -1,5 +1,6 @@
 const {default: axios} = require ("axios");
 const {Staffs, Process} = require("../db");
+const { Op } = require("sequelize");
 
 async function createStaffDb(req, res){
     try{
@@ -34,7 +35,7 @@ async function createStaffDb(req, res){
         console.log(error)
     }
 }
-
+// funcion para enviar todo  el staff
 const getAllStaff= async(req,res)=>{
     try {
        let allStaffs =  await Staffs.findAll({
@@ -50,4 +51,111 @@ const getAllStaff= async(req,res)=>{
     } 
 }
 
-module.exports = {createStaffDb, getAllStaff}
+// funcion para enviar el detalle del personal por id
+const getStaffById = async (req, res)=>{
+    let {id} = req.params;
+    const staffById = await Staffs.findOne({
+        include:[{
+            model:Process,
+            attribute:["description"]
+        }],
+        where:{id:id}
+    })
+    res.status(200).send(staffById);
+}
+
+// funcion para buscar el personal por el nombre
+const getStaffByName= async(req,res)=>{
+    const {name}= req.query;
+    if(name){
+        try {
+            let staffByName = await Staffs.findAll({
+                include:[{
+                    model:Process,
+                    attribute:["description"]
+                }],
+                where:{
+                    name:{[Op.iLike ]: name +'%'},
+                }
+            })
+            staffByName.length?
+            res.status(200).send(staffByName): res.status(404).send("No existe registro de la persona a buscar")
+        } catch (error) {
+            console.log(error)           
+        }
+    }
+
+
+}
+
+const postStaff = async (req, res)=>{
+    let { name,avatar, document,addres,phone} = req.body;
+    try {
+        const staff = {name,avatar, document,addres,phone}
+        if(isNaN(name)===false)
+        return res.send("El valor ingresado no debe ser numerico");
+        if(!name || !avatar || !document || !addres ||  !phone) res.send("Falta informacion");
+        const validate = await Staffs.findOne({
+            where:{document},
+        })
+        if(!validate){
+            let newStaff = await Staffs.create(staff);
+            res.status(200).send(`La persona ${staff} con numero de documento ${document} se registro correctamente`)
+        }else{
+            res.status(400).send(`No se puede registrar la persona ${staff} porque ya existe el numero de documento ${document}en la base de datos`)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const putStaff = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const {
+        name,avatar, document,addres, phone} = req.body;
+      const editStaff = await Staffs.update(
+        {
+            name,avatar, document,addres, phone
+        },
+        { where: { id: id } }
+      );
+      res.send(`Se ha modificado la persona con id ${editStaff}`);
+    } catch (error) {
+      return error;
+    }
+  };
+  
+  const deleteStaff = async (req, res) => {
+    try {
+      const id = req.params.id;
+      await Staffs.destroy({
+        where: { id: id },
+      });
+      return res.send(" Staff deleted!");
+    } catch (error) {
+      return error;
+    }
+  };
+  
+  const restoreStaff = async(req, res) => {
+      let { id } = req.params;
+      try {
+          await Staffs.restore({
+              where: {
+                  id: id
+              }
+          });
+          const restoreStaff = await Staffs.findOne({
+              where: {
+                  id: id
+              }
+          })
+          res.status(200).send(`La persona con id ${id} se ha restaurado con exito`)
+      } catch (error) {
+          res.status(400).send('Hubo un problema recuperando el usuario')
+      }
+  }
+
+
+module.exports = {createStaffDb, getAllStaff, getStaffById, getStaffByName, postStaff, putStaff,deleteStaff, restoreStaff}
--- api/src/routes/routerStaffs.js
+++ api/src/routes/routerStaffs.js
@@ -1,11 +1,17 @@
 const axios = require ('axios')
-const {getAllStaff} = require('../controllers/staff')
+const {getAllStaff, getStaffById, getStaffByName, postStaff, putStaff,deleteStaff, restoreStaff} = require('../controllers/staff')
 
 const express = require('express')
 const router= express.Router();
 // const {Staffs} = require ('../db')
 
 router.get('/', getAllStaff)
+router.get('/search/', getStaffByName)
+router.post('/', postStaff)
+router.put('/edit/:id',putStaff)
+router.delete('/delete/:id',deleteStaff)
+router.get('/restore/:id', restoreStaff)
+router.get('/:id',getStaffById )
 
 
 module.exports=router;
